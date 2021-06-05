9af1f0ca9df023b0944e6d52c5dec5f3c6d1b174
ISHU <rekhaishu121299@gmail.com>
2021-06-06 00:09:09 +0530

    "day5-2"

--- Functions_and_Arrays/allSubsets.java
+++ Functions_and_Arrays/allSubsets.java
@@ -9,18 +9,18 @@ public class allSubsets {
             arr[i] = scn.nextInt();
         scn.close();
 
-        for (int i = 0; i < Math.pow(2, n); i++) {
+        for (int i = 0; i < (int) Math.pow(2, n); i++) {
             int b = toBinary(i);
             int mul = n - 1;
             int idx = 0;
-            while (b > 0) {
+            while (idx < n) {
                 int r = b / (int) Math.pow(10, mul);
                 b = b % (int) Math.pow(10, mul);
-                mul++;
+                mul--;
                 if (r != 0)
-                    System.out.println(arr[idx] + "\t");
+                    System.out.print(arr[idx] + "\t");
                 else
-                    System.out.println("\t");
+                    System.out.print("-\t");
                 idx++;
             }
             System.out.println();
@@ -28,11 +28,12 @@ public class allSubsets {
     }
 
     public static int toBinary(int n) {
-        int ans = 0, mul = 0;
-        while (n > 0) {
-            int r = n % 2;
-            n = n / 2;
-            ans += r * (int) Math.pow(10, mul);
+        int ans=0,mul=0;
+        while(n>0)
+        {
+            int r = n%2;
+            n=n/2;
+            ans+= r*(int)Math.pow(10,mul);
             mul++;
         }
         return ans;
